83b98f39eb5c9947cc8c85fdcfd6c6cbd3161d0d
cjustinn <such.justinn@gmail.com>
2022-09-29 15:36:41 +0100

    Updated createnews route

--- pages/api/createnews.js
+++ pages/api/createnews.js
@@ -22,9 +22,16 @@ export default async function handler(req, res) {
 
     if (!req.body.article) { res.status(400).json({ error: `You must provide article content.` }); }
     else {
-
+        
         prisma.newsArticle.create({
-            data: JSON.parse(req.body.article)
+            data: {
+                title: req.body.article.title,
+                author: req.body.article.author,
+                header: req.body.article.header,
+                preview: req.body.article.preview,
+                content: req.body.article.content,
+                displayLarge: req.body.article.displayLarge
+            }
         }).then(() => {
             res.status(201).json({ message: `Successfully created the article.` });
         }).catch(err => res.status(500).json({ error: err }));
